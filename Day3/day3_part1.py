0ce29386c6f8bc230fc2919bbe3452125d2de57e
Janine Hempy <janine@Davids-MacBook.local>
2021-12-04 08:55:40 -0500

    Day 3 done

--- Day3/day3_part1.py
+++ Day3/day3_part1.py
@@ -2,31 +2,37 @@
 
 from collections import Counter
 
-def parse(r):
+def get_bits(r):
     d = {}
     for i, c in enumerate(r):
         d[i] = int(c)
     return Counter(d)
 
-def count_bits(file):
+def parse(file):
     lines = 0
-
     bit_counts = Counter()
-
     f = open(file, "r")
     for reading in f:
         lines = lines + 1
-        reading_counter = parse(reading.strip())
+        reading_counter = get_bits(reading.strip())
         bit_counts = bit_counts + reading_counter
-
     f.close()
-    return bit_counts
+    return bit_counts, lines
 
 def calculate_rates(file):
-    bit_counts = count_bits(file)
-
-    print(sorted(bit_counts.items()))
-    return 1,2
+    gamma_binary = '';
+    epsilon_binary = ''
+    (bit_counts, line_count) = parse(file)
+    for (bit, count) in sorted(bit_counts.items()):
+        if (count > line_count/2):
+            gamma_binary = gamma_binary + '1'
+            epsilon_binary = epsilon_binary + '0'
+        else:
+            gamma_binary = gamma_binary + '0'
+            epsilon_binary = epsilon_binary + '1'
+    # print(gamma_binary)
+    # print(epsilon_binary)
+    return int(gamma_binary, 2), int(epsilon_binary, 2)
 
 
 def calculate_power_consumption(file):
@@ -43,4 +49,4 @@ def test():
     print(parse('101010'))
 
 # test()
-main('test.txt')
+main('input.txt')
--- Day3/day3_part2.py
+++ Day3/day3_part2.py
@@ -1,2 +1,69 @@
 # day3_part2.py
 
+from collections import Counter
+
+def get_bits(r):
+    d = {}
+    for i, c in enumerate(r):
+        d[i] = int(c)
+    return Counter(d)
+
+def parse(readings):
+    bit_counts = Counter()
+    for reading in readings:
+        reading_counter = get_bits(reading.strip())
+        bit_counts = bit_counts + reading_counter
+    return bit_counts
+
+def slurp(file):
+    readings = []
+    f = open(file, "r")
+    for reading in f:
+        readings.append(reading.strip())
+    f.close()
+    return readings
+
+
+def keep_most_common(readings, position):
+	if (len(readings) == 1):
+		return readings[0]
+	else:
+		bit_counts = parse(readings)
+		target = 1 if (bit_counts[position] >= len(readings)/2) else 0
+		temp = list(filter(lambda x: int(x[position]) == target, readings))
+		return keep_most_common(temp, position + 1)
+
+
+def keep_least_common(readings, position):
+	if (len(readings) == 1):
+		return readings[0]
+	else:
+		bit_counts = parse(readings)
+		target = 0 if (bit_counts[position] >= len(readings)/2) else 1
+		temp = list(filter(lambda x: int(x[position]) == target, readings))
+		return keep_least_common(temp, position + 1)
+
+
+def calculate_oxygen_rating(file):
+	readings = slurp(file)
+	oxygen_rating = keep_most_common(readings, 0)
+	return int(oxygen_rating, 2)
+
+
+def calculate_co2_rating(file):
+	readings = slurp(file)
+	co2_rating = keep_least_common(readings, 0)
+	return int(co2_rating, 2)
+
+
+def calculate_life_support_rating(file):
+    oxygen_rating = calculate_oxygen_rating(file)
+    co2_rating = calculate_co2_rating(file)
+    return oxygen_rating * co2_rating
+
+def main(file):
+    life_support_rating = calculate_life_support_rating(file)
+    print(life_support_rating)
+
+main('input.txt')
+
