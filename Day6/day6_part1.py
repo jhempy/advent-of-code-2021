71ed46796b13b71035b1b6ad66b3ead94a7fe822
Janine Hempy <jhempy@gmail.com>
2021-12-06 09:08:56 -0500

    Oops, didn't make a new script for day 2 - fixed now

--- Day6/day6_part1.py
+++ Day6/day6_part1.py
@@ -1,8 +1,6 @@
 # day6_part1.py
 
-from collections import Counter
-
-def day_INEFFICIENT(fish):
+def day(fish):
 	new_population = []
 	babies = 0
 	for f in fish:
@@ -14,56 +12,24 @@ def day_INEFFICIENT(fish):
 	new_population += [8] * babies
 	return new_population
 
-def calculate_fish_population_INEFFICIENT(fish, days):
+def calculate_fish_population(fish, days):
 	yesterday_total = 0
 	for d in range(0, days):
 		yesterday_total = len(fish)
 		fish = day(fish)
 		today_total = len(fish)
 		print("Day: ", d, "Fish: ", today_total, "New: ", today_total - yesterday_total)
-		# print("Fish: ", fish)
-	return fish
-
-def parse_input_file_INEFFICIENT(file):
-	fish = []
-	f = open(file, "r")
-	for line in f:
-		t = list(map(int, line.split(',')))
-		fish = fish + t
-	f.close()
-	return fish
-
-def day(fish):
-	new_fish = Counter()
-	babies = 0
-	for k in fish.keys():
-		v = fish[k]
-		if k == 0:
-			new_fish[8] += v
-			new_fish[6] += v
-		else:
-			new_fish[k-1] += v
-	return(new_fish)
-
-
-def calculate_fish_population(fish, days):
-	for d in range(0, days):
-		yesterday_total = sum(fish.values())
-		fish = day(fish)
-		today_total = sum(fish.values())
-		print("Day: ", d, ", Fish: ", today_total, ", New: ", today_total - yesterday_total)
 		print("Fish: ", fish)
 	return fish
 
-
-def parse_input_file(file): 
+def parse_input_file(file):
 	fish = []
 	f = open(file, "r")
 	for line in f:
 		t = list(map(int, line.split(',')))
 		fish = fish + t
 	f.close()
-	return Counter(fish)
+	return fish
 
 def main(file):
 	print("Starting...")
@@ -71,12 +37,12 @@ def main(file):
 	fish = parse_input_file(file)
 	print("Fish start as: ")
 	print(fish)
-	days = 256
+	days = 18
 	fish = calculate_fish_population(fish, days)
 	print("Fish end as: ")
 	print(fish)
 	print("Fish count is now: ")
-	print(sum(fish.values()))
+	print(len(fish))
 	print("Done!")
 
-main('input.txt')
+main('test.txt')
--- Day6/day6_part2.py
+++ Day6/day6_part2.py
@@ -1,1 +1,50 @@
 # day6_part2.py
+
+from collections import Counter
+
+def day(fish):
+	new_fish = Counter()
+	babies = 0
+	for k in fish.keys():
+		v = fish[k]
+		if k == 0:
+			new_fish[8] += v
+			new_fish[6] += v
+		else:
+			new_fish[k-1] += v
+	return(new_fish)
+
+
+def calculate_fish_population(fish, days):
+	for d in range(0, days):
+		yesterday_total = sum(fish.values())
+		fish = day(fish)
+		today_total = sum(fish.values())
+		print("Day: ", d, ", Fish: ", today_total, ", New: ", today_total - yesterday_total)
+		print("Fish: ", fish)
+	return fish
+
+
+def parse_input_file(file): 
+	fish = []
+	f = open(file, "r")
+	for line in f:
+		t = list(map(int, line.split(',')))
+		fish = fish + t
+	f.close()
+	return Counter(fish)
+
+def main(file):
+	print("Starting...")
+	fish = parse_input_file(file)
+	print("Fish start as: ")
+	print(fish)
+	days = 256
+	fish = calculate_fish_population(fish, days)
+	print("Fish end as: ")
+	print(fish)
+	print("Fish count is now: ")
+	print(sum(fish.values()))
+	print("Done!")
+
+main('input.txt')
