c1b382010516b60e6cc9912a6871bb76719b8059
Janine Hempy <jhempy@gmail.com>
2021-12-20 09:28:57 -0500

    Ready for 20

--- Day19/day19_part1.py
+++ Day19/day19_part1.py
@@ -2,19 +2,26 @@
 
 from collections import Counter
 
+scanners = []
+beacons = []
+
 def parse_input_file(file): 
+    global scanners
     f = open(file, "r")
+    sid = 0 # id of current scanner
+    bid = 0 
     for line in f:
         l = line.rstrip('\n')
         if l != '':
             if l[:3] == '---':
                 # new scanner
-                id = l.split(' ')[2]
-
-
-                print(id)
-
-
+                sid = l.split(' ')[2]
+                scanners.append(scanner(sid))
+            else:
+                # beacon for the current scanner
+                bid = bid + 1
+                x, y, z = list(map(int, l.split(',')))
+                beacons.append(beacon(bid, sid, x, y, z))
 
     f.close()
     return
@@ -23,30 +30,42 @@ class scanner:
 
     def __init__(self, id):
         self.id = id
+        self.location = ''
+        self.direction = ''
         self.beacons = []
 
     def __str__(self):
         out =       f'----------------' + '\n'
-        out = out + f'Scanner Object: {self.id}' + '\n'
+        out = out + f'Scanner {self.id}' + '\n'
         out = out + f'----------------' + '\n'
         return out
 
 class beacon:
 
-    def __init__(self, id):
-        self.id = id
+    def __init__(self, bid, sid, x, y, z):
+        self.id = sid
+        d = {'sid': sid, 'x': x, 'y': y, 'z': z}
+        self.distances = []
+        self.distances.append(d)
+        self.neighbors = []
 
     def __str__(self):
         out =       f'----------------' + '\n'
         out = out + f'Beacon Object: {self.id}' + '\n'
+        out = out + f'  Distances: {self.distances}' + '\n'
         out = out + f'----------------' + '\n'
         return out
-
-
 
 def main(file):
 
     parse_input_file(file)
+
+
+    for s in scanners:
+        print(s)
+
+    for b in beacons:
+        print(b)
 
 
     # Cleanup
@@ -56,3 +75,4 @@ def main(file):
 
 main('test.txt')
 # main('input.txt')
+
